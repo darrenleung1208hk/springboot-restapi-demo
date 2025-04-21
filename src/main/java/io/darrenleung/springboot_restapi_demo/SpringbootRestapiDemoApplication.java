743830b0b20c5d87b124e37caaed21654d7b99e1
Darren Leung <leungyatlong1208@gmail.com>
2025-04-21 17:42:22 +0800

    Initial commit: Spring Initializr project setup

---
+++ src/main/java/io/darrenleung/springboot_restapi_demo/SpringbootRestapiDemoApplication.java
@@ -0,0 +1,13 @@
+package io.darrenleung.springboot_restapi_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiDemoApplication.class, args);
+	}
+
+}
